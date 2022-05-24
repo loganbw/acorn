6f00fc94cea3a9efc0b8859e28a9c0fce2e939a3
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-24 09:49:37 -0400

    Installed SDK and Added GameCard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <section>
-    <div>
-      <router-view></router-view>
-    </div>
-  </section>
+  <header></header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <p>This Vites</p>
+  <RouterView />
 </template>
 <script>
   export default { components: {  } };
