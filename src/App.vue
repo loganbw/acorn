40bdd790625a840a5205e508887f8af4cd08a017
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-24 09:49:37 -0400

    Installed SDK and Added GameCard.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header></header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
     <p>This Vites</p>
   <RouterView />
