a2f0f5477aa2b8ebd3682b1465d59b1fdb9aad96
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-24 10:51:11 -0400

    Installed Vuex package and removed Vue Boilerplate items

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <header></header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <p>This Vites</p>
   <RouterView />
 </template>
 <script>
