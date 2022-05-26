4139b88f22cec4d91d9847c7688c2427014a2d03
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-26 14:29:06 -0400

    Added baseGameView and HandWidget.vue
    Added baseGameView and HandWidget to begin experimenting with a game layout and functional features (Draw, place card, attack, discard, etc.)

---
+++ src/components/widgets/HandWidget.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="playerHand">
+        <ul>
+            <li v-for="card in cards" :key="card.cardID" >
+                {{ card.cardName }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script> 
+    export default {
+        data() {
+            cards: [
+                {
+                    cardID: '',
+                    cardName: '',
+                    cardType: '',
+                    cardHP: '',
+                    cardAttacks: [
+                        {
+                            attackName: '',
+                            attackDamage:'',
+                            attackCost:''
+                        }
+                    ],
+                    cardAbilities: [
+                        {
+                            abilityName: '',
+                            abilityDesc: '',
+                            abilityCalc:''
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
