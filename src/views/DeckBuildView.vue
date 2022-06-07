c3a2f580e90c3d06465cad19c90fededef101473
Logan W <loganbryantweathersby@gmail.com>
2022-06-07 11:01:15 -0400

    adding html structure for the deck view

--- src/views/DeckBuildView.vue
+++ src/views/DeckBuildView.vue
@@ -1,11 +1,21 @@
 <template >
-    <div>
-        
+    <BaseHeader />
+    <div class="contain">
+        <div class="searchContain">
+            
+        </div>
+        <div class="cardContain">
+
+        </div>
+        <div class="deckContain">
+            
+        </div>
     </div>
 </template>
 <script>
+import BaseHeader from "../components/UI/baseHeader.vue"
 export default {
-    
+    components:{BaseHeader}
 }
 </script>
 <style scoped>
