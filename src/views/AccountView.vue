e4135dca1e9518b4b0541c9398c741a4ba09be4a
Logan W <loganbryantweathersby@gmail.com>
2022-06-23 08:34:27 -0400

    added a test npm package, added a header test file, added avatars so users can change their img

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -9,15 +9,16 @@
     <div class="containAccount">
       <div class="account">
         <div class="imgContainer">
-          <img class="avatar" src="../assets/defaultAvatar.svg" />
+          <img class="avatar" :src="getAvatarImg" />
           <div class="avatarEdit">
-            <img class="editPen" src="../assets/pencil.svg" />
+            <label class="editPen" for="photo">Change</label>
+            <input type="file" class="uploadFile" id="photo" @change="getFile" />
           </div>
         </div>
-        <form>
-          <input v-if="edit" type="text" name="uName" />
-          <div class="formEdits" v-else>
-            <label for="uName"> User Name</label>
+        <form @submit.prevent="changeUserName">
+          <input v-if="edit" type="text" placeholder="User Name" v-model="userName" name="uName" />
+          <div @click="changeToEdit" class="formEdits" v-else>
+            <label for="uName"> {{ displayUser }}</label>
             <img class="editSvg" src="../assets/pencil.svg" />
           </div>
         </form>
@@ -26,19 +27,18 @@
         <h3>Decks</h3>
         <ul class="decks">
           <li class="deck" v-for="deck in getDecks">
-            <span>{{deck.deckName}}</span>
+            <span @click="deleteDeck">{{ deck.deckName }}</span>
             <img src="" />
-            <img src="" />
+            <img class="exitSvg" src="../assets/exit.svg" @click="deleteDeck(deck)" />
           </li>
         </ul>
-        
       </div>
     </div>
   </div>
 </template>
 <script>
   import BaseHeader from "../components/UI/baseHeader.vue";
-  import { getAllUsersDecks } from "../index.js";
+  import { getAllUsersDecks, deleteUserDeck, uploadImgFirebase, getAvatar } from "../index.js";
   import VueLoading from "vue-loading-overlay";
   export default {
     components: { BaseHeader, Loading: VueLoading },
@@ -46,22 +46,61 @@
       //this.$store.dispatch("fetchIsLoading", true);
     },
     mounted() {
-     
       this.$store.dispatch("fetchIsLoading", true);
-       //getAllUsersDecks(this.$store.getters.getUserData.data.uid);
+      //getAllUsersDecks(this.$store.getters.getUserData.data.uid);
     },
     data() {
       return {
         edit: false,
-        
+        userName: "",
+        imgFile: null,
       };
     },
-  
+    methods: {
+      deleteDeck(deck) {
+        deleteUserDeck(this.$store.getters.getUserData.data.uid, deck);
+      },
+      uploadImg() {
+        this.$store.dispatch("fetchIsLoading", true);
+        uploadImgFirebase(this.imgFile);
+      },
+    },
+
     computed: {
-       getDecks(){
-        return  this.$store.getters.getAllUsersDecks
+      getAvatarImg() {
+        return this.$store.getters.getAvatar;
+      },
+      getUserName(){
+        //dispatch store value to set it to username
+      }
+      getDecks() {
+        return this.$store.getters.getAllUsersDecks;
         //getAllUsersDecks(this.$store.getters.getUserData.data.uid);
-       }
+      },
+      changeToEdit() {
+        return (this.edit = !this.edit);
+      },
+      displayUser() {
+        if (this.userName == "") return "User Name";
+        else return this.userName;
+      },
+
+      changeUserName() {
+        //call to change u-name
+        console.log(this.userName);
+        this.edit = !this.edit;
+      },
+      getFile() {
+        this.imgFile = event.target.files[0];
+        console.log(this.imgFile);
+        if (
+          this.imgFile.name.includes(".png") ||
+          this.imgFile.name.includes(".jpg") ||
+          this.imgFile.name.includes(".jpeg")
+        ) {
+          this.uploadImg();
+        }
+      },
     },
   };
 </script>
@@ -86,6 +125,7 @@
   }
   .editPen {
     width: 80%;
+    margin-top: 25%;
   }
   .account {
     display: flex;
@@ -109,6 +149,22 @@
     display: flex;
     justify-content: center;
   }
+  .load {
+    position: relative;
+    /* margin-top: -10%; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    width: 100%;
+    bottom: 0;
+    opacity: 0.16;
+    position: fixed;
+    top: 0;
+
+    z-index: 1;
+  }
+
   .containAccount {
     display: flex;
     flex-direction: column;
@@ -131,6 +187,7 @@
     align-items: center;
     align-content: center;
     justify-content: center;
+    text-align: center;
   }
   li {
     list-style: none;
@@ -141,5 +198,16 @@
   .editSvg {
     width: 10%;
     margin-left: 2%;
+  }
+  .exitSvg {
+    width: 20%;
+    color: #c90e0e;
+  }
+  .uploadFile {
+    visibility: hidden;
+    width: 20%;
+  }
+  .uploadButton {
+    visibility: hidden;
   }
 </style>
